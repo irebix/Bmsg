6fa19476575312ed829145e5a7d869f79c87718a
rebix <irebix@qq.com>
2023-02-05 05:45:51 +0800

    copy

--- quickR.js
+++ quickR.js
@@ -7,25 +7,53 @@
 // @license      MIT
 // @match        https://message.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        GM_setClipboard
 // ==/UserScript==
 
 //延时加载
 setTimeout(
     function() {
         'use strict';
-        //遍历所有消息框，
+        //遍历所有消息框
         document.querySelectorAll('.send-box').forEach(qs => {
             console.log(qs);
             //创建按钮和备选项box
             let btn=document.createElement("button");
             btn.innerText="快捷回复";
-            btn.style = "height: 30px";
-
-
+            btn.style = "height: 40px";
 
             let box = document.createElement("box");
-            box.style = "height: 200px; display: none; text-align: center; line-height: 50px";
-            box.innerText = "text"
+            box.style = "height: 200px; display: none; text-align: center; vertical-align: middle;";
+
+            textContent("(°∀°)ﾉhi");
+            textContent("号左右会有一版草稿，那我先接历");
+            textContent("久等历，您看看这是草稿阶段，有问题请尽管提‍[兔年]");
+            textContent("您看看这是完成稿，有问题也请尽管提(=・ω・=)");
+            textContent("感谢关照~方便的话请赏一个评论吧嘿嘿(*°▽°*)八(*°▽°*)♪");
+
+            //创建快捷回复内容
+            function textContent(content){
+                    
+                let text = document.createElement("button");
+                text.innerHTML = content;
+                text.style = "height: 40px;";
+                text.onclick=function(){
+                    //点按
+                    GM.setClipboard(content)
+                    box.style.display="none";
+                    boxDisplay = true;
+                    let input = document.querySelector(".core-style");
+                    //input.innerHTML = content;
+                    input.value = content;
+
+                };
+                
+                box.appendChild(text);
+                //添加一个换行
+                var br = document.createElement("br");
+                box.appendChild(br);
+            }
+            
 
             //设定默认box不显示
             let boxDisplay = true;
