19911d61a11a189261077345685e27a4ef0388d5
rebix <irebix@qq.com>
2023-02-13 20:12:44 +0800

    version change

--- quickR.js
+++ quickR.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         哔哩私信快捷回复
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  哔哩私信快捷回复菜单
 // @author       rebix
 // @license      MIT
